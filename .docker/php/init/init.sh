50270b8e4c3650d855472a8a0241bfab809db52f
Paul <mmelnikov@live.de>
2021-10-04 13:33:25 +0200

    int.sh - if else  statement fix

--- .docker/php/init/init.sh
+++ .docker/php/init/init.sh
@@ -3,19 +3,23 @@
 DIR_VENDOR="/var/www/vendor";
 DIR_NODE_MODULES="/var/www/node_modules";
 
-if [ -z "$(ls -A /var/www)" ]; then
-    if [ -z "${APP_REPOSITORY}" ]; then
+if [ -z "$(ls -A /var/www)" ] 
+then
+    if [ -z "${APP_REPOSITORY}" ]
+    then
         composer create-project laravel/laravel .
     else 
         git clone "${APP_REPOSITORY}" .
     fi
 fi
 
-if [ ! -d "$DIR_VENDOR" ];  then    
+if [ ! -d "$DIR_VENDOR" ] 
+then    
     composer install       
 fi
 
-if [ ! -d "$DIR_NODE_MODULES" ]; then
+if [ ! -d "$DIR_NODE_MODULES" ]
+then
     npm install  
 fi
 
