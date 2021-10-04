5c5eebbf9b616f822b982bfccf4eda16afc8593f
Paul <mmelnikov@live.de>
2021-10-04 05:25:43 +0200

    init.sh - laravel installation

--- .docker/php/init/init.sh
+++ .docker/php/init/init.sh
@@ -3,9 +3,15 @@
 DIR_VENDOR="/var/www/vendor";
 DIR_NODE_MODULES="/var/www/node_modules";
 
-if [ ! -d "$DIR_VENDOR" ] || [ ! -d "$DIR_NODE_MODULES" ]; then
-    rm -rf /var/www/*
-    composer create-project laravel/laravel .
+if [ -z "$(ls -A /var/www)" ]; then
+    if [ -z "${APP_REPOSITORY}" ]; then
+        composer create-project laravel/laravel .
+    else 
+        git clone "${APP_REPOSITORY}" .
+    fi
+fi
+
+if [ ! -d "$DIR_VENDOR" ] || [ ! -d "$DIR_NODE_MODULES" ]; then    
     composer install
     npm install         
 fi
